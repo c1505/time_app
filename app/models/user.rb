3a8c227cf06141147e6911e952d3e00a8b8d8e7e
c1505 <coreym247@gmail.com>
2015-08-16 20:54:41 +0000

    added audio and tests for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
     has_many :articles
     has_many :comments
     validates :name, :email, presence: true
+    validates :name, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i #regex for valid email is publicly available
+    validates :email, presence: true, length: { maximum: 150 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
